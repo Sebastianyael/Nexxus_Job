9d3370191eedc642232f27a54d902d52f0eabb27
Sebastianyael <sebastianyael963@gmail.com>
2026-03-19 21:56:56 -0600

    Configuraciones adicionales

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Configuramos los proxies para que Railway use HTTPS correctamente
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
